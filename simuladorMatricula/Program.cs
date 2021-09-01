b8a03a69a9d289122bf727e86a49d7d2bc141053
Diego F. Vasquez P <vapdif@gmail.com>
2021-08-31 20:21:21 -0500

    Agregar archivos de proyecto.

---
+++ simuladorMatricula/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simuladorMatricula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
