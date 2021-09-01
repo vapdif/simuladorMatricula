b84217a9c33f7f741503c7d9ba5f2e035671690d
Diego F. Vasquez P <vapdif@gmail.com>
2021-08-31 21:29:48 -0500

    new class

---
+++ simuladorMatricula/studens.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simuladorMatricula
+{
+    class studens
+    {
+        private string fName;
+        private int vCred, stratrum, nCred, vTotal, vDiscount, vNet;
+    }
+}
